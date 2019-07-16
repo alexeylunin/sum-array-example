92c65b070361d94c66502f6fbf3953cf2365f306
Alexey Lunin <alexey.lunin0@gmail.com>
2019-07-16 08:20:25 +0300

    Added application basis. Applied prettier.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import findSum from "./utils/findSum";
 
 function App() {
+  const [value, setValue] = useState("[0,1,2,3,4,5]");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
+
+  const calculateResult = () => {
+    setError(null);
+    let array = [];
+    try {
+      array = JSON.parse(value);
+      if (typeof array.length !== "number") {
+        setError("Please enter valid array");
+        return;
+      }
+    } catch (e) {
+      setError(
+        "Please check the text in the textarea field. There must paste correct array value"
+      );
+      return;
+    }
+    const sum = findSum(array);
+    setResult(sum);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <textarea
+            value={value}
+            onChange={e => {
+              setValue(e.target.value);
+            }}
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Example: [0,1,2,3,4,5]</div>
+        {error && <div className="App-error">{error}</div>}
+        <button className="App-button" onClick={calculateResult}>
+          Calculate
+        </button>
+        {result && <div>Result: {result}</div>}
       </header>
     </div>
   );
---
+++ src/utils/findSum.js
@@ -0,0 +1,5 @@
+const findSum = arr => {
+  return 0;
+};
+
+export default findSum;
